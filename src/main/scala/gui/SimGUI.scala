05fa5d19f4589e765a0fb7068f1c5ebb190c00ff
Alessandro Stefani <sandostef@gmail.com>
2025-06-17 15:55:09 +0200

    PS-10: buttons spaced better

--- src/main/scala/gui/SimGUI.scala
+++ src/main/scala/gui/SimGUI.scala
@@ -3,6 +3,7 @@ package gui
 import gui.SimGUI.stage
 import scalafx.application.JFXApp3
 import scalafx.geometry.Insets
+import scalafx.geometry.Pos.{BottomCenter, BottomLeft, BottomRight, Center, TopLeft, TopRight}
 import scalafx.scene.Scene
 import scalafx.scene.control.Button
 import scalafx.scene.layout.{BorderPane, HBox, Pane, VBox}
@@ -13,11 +14,13 @@ import scalafx.stage.Screen
 object SimGUI extends JFXApp3{
 
   override def start():Unit = {
-    val X = Screen.primary.bounds.width
-    val Y = Screen.primary.bounds.height
+    val X = Screen.primary.visualBounds.width
+    val Y = Screen.primary.visualBounds.height
 
     // parte superiore con la mappa del mondo
-    val top = new BorderPane {
+    val map = new HBox() {
+      alignmentInParent = Center
+      margin = Insets(Y/3,X/3,Y/3,X/3)
       children = new Text {
 
         text = "Mappa del Mondo"
@@ -25,29 +28,38 @@ object SimGUI extends JFXApp3{
     }
 
     // parte inferiore con i pulsanti ecc...
-    val plgBtn = new Button("Plague")
-    val wrldBtn = new Button("World")
+    val plgBtn = new Button {
+      padding = Insets(20, 60, 20, 60)
+      text = "Plague"
+    }
+    
+
+    val wrldBtn = new Button {
+      padding = Insets(20, 60, 20, 60)
+      text = "World"
+    }
+
     // potenzialmente da aggiungere le info di diffusione
 //  val info = ???
 
-    val bot = new HBox {
-      children = Seq(plgBtn, wrldBtn)
+    val btns = new BorderPane() {
+      alignmentInParent = BottomCenter
+      left = plgBtn
+      right = wrldBtn
     }
 
     // layout principale
-    val mainLayout = new VBox {
-      children = Seq(top, bot)
+    val mainLayout = new BorderPane {
+      center = map
+      bottom = btns
     }
 
     val mainScene = new Scene {
-      fill = Color.rgb(204, 198, 108)
       content = mainLayout
     }
 
     stage = new JFXApp3.PrimaryStage {
       title = "Plague Sim"
-      height = Y
-      width = X
       scene = mainScene
     }
   }
