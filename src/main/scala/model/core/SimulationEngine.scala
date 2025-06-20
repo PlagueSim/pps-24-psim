edea0bfac24dac23d85f56478b6a9788ce5d1702
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-06-20 12:39:24 +0200

    PS-3 Event definition and implementation, refactoring

--- src/main/scala/model/core/SimulationEngine.scala
+++ src/main/scala/model/core/SimulationEngine.scala
@@ -1,18 +1,19 @@
 package model.core
 
 import cats.data.State
-import cats.syntax.all._
+import cats.syntax.all.*
+import model.events.{AdvanceDayEvent, Event}
 
 object SimulationEngine:
-  private type Simulation[A] = State[SimulationState, A]
+  type Simulation[A] = State[SimulationState, A]
 
-  def advanceDay(): Simulation[Int] =
-    State
-      .modify[SimulationState](state => SimulationState(state.currentDay + 1))
-      .inspect(x => x.currentDay)
-
-  def advanceFor(days: Int): Simulation[Int] =
-    List.fill(days)(advanceDay()).sequence.map(_.lastOption.getOrElse(-1))
+//  def advanceDay(): Simulation[Int] =
+//    State
+//      .modify[SimulationState](state => SimulationState(state.currentDay + 1))
+//      .inspect(x => x.currentDay)
+//
+//  def advanceFor(days: Int): Simulation[Int] =
+//    List.fill(days)(advanceDay()).sequence.map(_.lastOption.getOrElse(-1))
 //    (1 to days).foldLeft(State.inspect[SimulationState, Int](_.currentDay)) {
 //      (acc, _) => acc.flatMap(_ => advanceDay())
 //    }
@@ -20,3 +21,22 @@ object SimulationEngine:
 //    case 0 => State.inspect(_.currentDay)
 //    case n if n > 0 =>
 //      advanceDay().flatMap(_ => advanceFor(n - 1))
+
+  def executeEvent[A](event: Event[A]): Simulation[A] = event.execute()
+
+  def executeListOfEvents[A](events: List[Event[A]]): Simulation[List[A]] =
+    events.traverse(executeEvent)
+
+  def runStandardSimulation(): Unit =
+    val listOfEvents =
+      List(AdvanceDayEvent(), AdvanceDayEvent(), AdvanceDayEvent())
+    val initialState           = SimulationState(0)
+    val cycle: Simulation[Int] = for
+      _   <- executeEvent(AdvanceDayEvent())
+      _   <- executeEvent(AdvanceDayEvent())
+      _   <- executeListOfEvents(listOfEvents)
+      end <- executeEvent(AdvanceDayEvent())
+    yield end
+
+    val endSim = cycle.runA(initialState).value
+    println(s"Simulation ended on day: $endSim")
--- src/main/scala/model/events/Event.scala
+++ src/main/scala/model/events/Event.scala
@@ -2,15 +2,14 @@ package model.events
 
 import cats.data.State
 import model.core.SimulationEngine.Simulation
-import model.core.SimulationState
 
 trait Event[A] {
   def execute(): Simulation[A]
 }
 
-case class AdvanceDayEvent() extends Event[Int] {
+case class AdvanceDayEvent() extends Event[Int]:
   override def execute(): Simulation[Int] =
-    State
-      .modify[SimulationState](s => SimulationState(s.currentDay + 1))
-      .inspect(_.currentDay)
-}
+    State { s =>
+      val updatedState = s.copy(currentDay = s.currentDay + 1)
+      updatedState -> updatedState.currentDay
+    }
