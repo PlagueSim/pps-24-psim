78ce32ee7c794aed704c8b5562068d4e58516af3
Alessandro Stefani <sandostef@gmail.com>
2025-06-22 19:13:39 +0200

    PS-10: added more pages to PlagueInfo

--- src/main/scala/view/PlagueView.scala
+++ src/main/scala/view/PlagueView.scala
@@ -1,13 +1,27 @@
 package view
 
 import scalafx.geometry.Insets
-import scalafx.scene.layout.{BorderPane, HBox, Priority}
+import scalafx.scene.layout.{BorderPane, HBox, Priority, VBox}
 import scalafx.scene.text.Text
- //TODO
-class PlagueView extends BorderPane {
-   val plagueInfos = new Text("SCALABBIAA!!!")
-   HBox.setHgrow(plagueInfos, Priority.Always)
+import controller.PlagueViewController
 
-   padding = Insets(10)
-   center = plagueInfos
-}
+class PlagueView extends BorderPane:
+  private val controller = new PlagueViewController(this)
+
+  val plagueInfos = new Text("EH")
+  HBox.setHgrow(plagueInfos, Priority.Always)
+
+
+
+  private val trsBtn = StdBtn("Transmission"):
+    controller.show(TransmissionView())
+  private val smptsBtn = StdBtn("Symptoms"):
+    controller.show(SymptomsView())
+  private val ablBtn = StdBtn("Abilities"):
+    controller.show(AbilityView())
+  private val leftSide = new VBox:
+    children = Seq(trsBtn, smptsBtn, ablBtn)
+
+  padding = Insets(10)
+  center = plagueInfos
+  left = leftSide
