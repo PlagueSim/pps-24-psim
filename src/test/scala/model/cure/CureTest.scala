58792359255c0afb957a4d8ebe995c46d0fb31ec
Matteo Susca <matteo.susca@studio.unibo.it>
2025-06-26 22:49:44 +0200

    PS-22: reformat

--- src/test/scala/model/cure/CureTest.scala
+++ src/test/scala/model/cure/CureTest.scala
@@ -2,7 +2,6 @@ package model.cure
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.matchers.should.Matchers.exist.and
 
 class CureTest extends AnyFlatSpec with Matchers {
   "A Cure" should "have a default progress of 0.0" in {
@@ -10,18 +9,18 @@ class CureTest extends AnyFlatSpec with Matchers {
     cure.progress shouldEqual 0.0
   }
 
-    it should "have a default baseSpeed of 0.1" in {
-        val cure = Cure()
-        cure.baseSpeed shouldEqual 0.1
-    }
+  it should "have a default baseSpeed of 0.1" in {
+    val cure = Cure()
+    cure.baseSpeed shouldEqual 0.1
+  }
 
-    it should "allow setting a custom progress" in {
-        val cure = Cure(progress = 0.5)
-        cure.progress shouldEqual 0.5
-    }
+  it should "allow setting a custom progress" in {
+    val cure = Cure(progress = 0.5)
+    cure.progress shouldEqual 0.5
+  }
 
-    it should "allow setting a custom baseSpeed" in {
-        val cure = Cure(baseSpeed = 0.2)
-        cure.baseSpeed shouldEqual 0.2
-    }
+  it should "allow setting a custom baseSpeed" in {
+    val cure = Cure(baseSpeed = 0.2)
+    cure.baseSpeed shouldEqual 0.2
+  }
 }
