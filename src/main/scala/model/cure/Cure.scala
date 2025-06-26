15a822f5c337d4ba6fe19d6365ea2acb09e363c9
Matteo Susca <matteo.susca@studio.unibo.it>
2025-06-26 22:26:03 +0200

    PS-22: Cure and test

--- src/main/scala/model/cure/Cure.scala
+++ src/main/scala/model/cure/Cure.scala
@@ -1,5 +1,6 @@
 package model.cure
 
 final case class Cure(
-  progress: Double = 0.0
+    progress: Double = 0.0,
+    baseSpeed: Double = 0.1
 )
--- src/test/scala/model/cure/CureTest.scala
+++ src/test/scala/model/cure/CureTest.scala
@@ -4,9 +4,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class CureSpec extends AnyFlatSpec with Matchers {
-
-  "A Cure" should "be creatable with default values and have initial progress 0.0, speed 0.01, and no difficulty factors" in {
+  "A Cure" should "have a default progress of 0.0 and baseSpeed of 0.1" in {
     val cure = Cure()
-    cure.progress shouldBe 0.0
+    cure.progress shouldEqual 0.0
+    cure.baseSpeed shouldEqual 0.1
+  }
+
+  it should "allow setting custom progress and baseSpeed values" in {
+    val cure = Cure(progress = 0.5, baseSpeed = 0.2)
+    cure.progress shouldEqual 0.5
+    cure.baseSpeed shouldEqual 0.2
   }
 }
