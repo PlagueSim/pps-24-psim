3b4c6168384379c2ee7f4bf69ed7f8ba69c14b9c
Matteo Susca <matteo.susca@studio.unibo.it>
2025-06-26 22:32:20 +0200

    PS-22: changed cure test name

--- src/test/scala/model/cure/CureTest.scala
+++ src/test/scala/model/cure/CureTest.scala
@@ -3,7 +3,7 @@ package model.cure
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class CureSpec extends AnyFlatSpec with Matchers {
+class CureTest extends AnyFlatSpec with Matchers {
   "A Cure" should "have a default progress of 0.0 and baseSpeed of 0.1" in {
     val cure = Cure()
     cure.progress shouldEqual 0.0
