436a35c4a094a259f0456b3b0bbde2728af2802d
Andrea Zavatta <zavattaandrea@gmail.com>
2025-06-30 15:10:38 +0200

    PS-18: add check on creation of world

--- src/main/scala/model/World/World.scala
+++ src/main/scala/model/World/World.scala
@@ -7,5 +7,21 @@ case class World private(
                         )
 
 object World:
+
   def empty: World =
     World(Map.empty, Set.empty, Map.empty)
+
+  def apply(
+             nodes: Map[String, Node],
+             edges: Set[Edge],
+             movements: Map[String, MovementStrategy]
+           ): World =
+    require(
+      edges.forall(e => nodes.contains(e.nodeA) && nodes.contains(e.nodeB)),
+      "Edges must connect existing nodes"
+    )
+    require(
+      movements.keySet.subsetOf(nodes.keySet),
+      "Movements must refer to existing nodes"
+    )
+    new World(nodes, edges, movements)
--- src/test/scala/World/WorldTest.scala
+++ src/test/scala/World/WorldTest.scala
@@ -1,6 +1,8 @@
 package World
 
-import model.World.World
+import model.World.World.*
+import model.World.*
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -13,5 +15,48 @@ class WorldTest extends AnyFlatSpec with Matchers:
     world.movements shouldBe empty
   }
 
+  "World.apply" should "validate that edges connect existing nodes" in {
+    val node = Node.withPopulation(10).build()
+    val nodes = Map("A" -> node)
+    val edges = Set(Edge("A", "B"))
+    val movements = Map("A" -> Static)
 
+    an[IllegalArgumentException] shouldBe thrownBy {
+      World(nodes, edges, movements)
+    }
+  }
 
+  it should "validate that movements target existing nodes" in {
+    val node = Node.withPopulation(10).build()
+    val nodes = Map("A" -> node)
+    val edges = Set.empty[Edge]
+    val movements = Map("B" -> Static)
+
+    an[IllegalArgumentException] shouldBe thrownBy {
+      World(nodes, edges, movements)
+    }
+  }
+
+  it should "create a valid World when all nodes, edges, and movements are consistent" in {
+    val nodeA = Node.withPopulation(10).build()
+    val nodeB = Node.withPopulation(5).build()
+
+    val nodes = Map(
+      "A" -> nodeA,
+      "B" -> nodeB
+    )
+    val edges = Set(
+      Edge("A", "B"),
+      Edge("B", "A")
+    )
+    val movements = Map(
+      "A" -> Static,
+      "B" -> Static
+    )
+
+    val world = World(nodes, edges, movements)
+
+    world.nodes shouldBe nodes
+    world.edges shouldBe edges
+    world.movements shouldBe movements
+  }
