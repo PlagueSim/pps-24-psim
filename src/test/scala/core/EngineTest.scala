38adeb832c33b62297c8ca7810418795f66883e2
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-07-02 12:44:08 +0200

    PS-28 green tests

--- src/test/scala/core/EngineTest.scala
+++ src/test/scala/core/EngineTest.scala
@@ -4,48 +4,47 @@ import model.core.{SimulationEngine, SimulationState}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import model.events.*
+import model.time.Time
+import model.time.BasicYear
+import model.time.TimeTypes.*
 
-class EngineTest extends AnyFlatSpec with Matchers {
+class EngineTest extends AnyFlatSpec with Matchers:
 
   "The simulationState" should "contains all the vital component of the simulation, such as the World, the Virus," +
-    "the Days" in :
-      val simState = SimulationState(0)
-      simState.currentDay shouldBe a[Int]
+    "the Days" in {
+      val simState = SimulationState(BasicYear(Day(0), Year(2023)))
+      simState.time shouldBe a[Time]
+    }
 
+  it should "be able to read the current day" in {
+    val simState = SimulationState(BasicYear(Day(5), Year(2023)))
+    simState.time.day.value shouldEqual 5
+  }
 
-  it should "be able to read the current day" in :
-    val simState = SimulationState(5)
-    simState.currentDay shouldEqual 5
-
-
-  "The engine" should "be able to execute a simulation step and returning the updated currentDay value" in :
-    val simState  = SimulationState(0)
+  "The engine" should "be able to execute a simulation step and returning the updated time value" in:
+    val simState  = SimulationState(BasicYear(Day(0), Year(2023)))
     val nextState =
       for nextState <- SimulationEngine.executeEvent(AdvanceDayEvent())
       yield nextState
-    nextState.run(simState).value._2 shouldEqual 1
+    nextState.run(simState).value._2.day.value shouldEqual 1
 
-
-  "An event " should "have the execute method" in :
-    val simState                    = SimulationState(0)
-    val advanceDayEvent: Event[Int] = AdvanceDayEvent()
+  "An event " should "have the execute method" in:
+    val simState = SimulationState(BasicYear(Day(0), Year(2023)))
+    val advanceDayEvent: Event[Time] = AdvanceDayEvent()
     val nextState = SimulationEngine.executeEvent(advanceDayEvent)
-    nextState.run(simState).value._2 shouldEqual 1
+    nextState.run(simState).value._2.day.value shouldEqual 1
 
-
-  it should "be able to call execute method and return the updated currentDay value" in :
-    val simState                    = SimulationState(0)
-    val advanceDayEvent: Event[Int] = AdvanceDayEvent()
-    val nextState                   = for
+  it should "be able to call execute method and return the updated currentDay value" in:
+    val simState = SimulationState(BasicYear(Day(0), Year(2023)))
+    val advanceDayEvent: Event[Time] = AdvanceDayEvent()
+    val nextState                    = for
       s1 <- SimulationEngine.executeEvent(advanceDayEvent)
       s2 <- SimulationEngine.executeEvent(advanceDayEvent)
     yield s2
-    nextState.run(simState).value._2 shouldEqual 2
+    nextState.run(simState).value._2.day.value shouldEqual 2
 
-
-  "The simulation engine" should "be able to run a standard simulation" in :
+  "The simulation engine" should "be able to run a standard simulation" in:
     SimulationEngine.runStandardSimulation()
-
 
   it should "print the correct end day" in:
     val stream = new java.io.ByteArrayOutputStream()
@@ -53,6 +52,3 @@ class EngineTest extends AnyFlatSpec with Matchers {
       SimulationEngine.runStandardSimulation()
     }
     stream.toString should include("Simulation ended on day: 6")
-
-
-}
