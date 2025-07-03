ff806efc1523ae4107886ffb7d5d8b4a9932c893
Andrea Zavatta <zavattaandrea@gmail.com>
2025-07-03 11:32:11 +0200

    add the position of the nodes dynamically based on the number of the nodes
    i added one nodes to the WorldFactory to test also with more nodes

--- src/main/scala/model/World/WorldFactory.scala
+++ src/main/scala/model/World/WorldFactory.scala
@@ -5,18 +5,24 @@ object WorldFactory:
     val nodeA = Node.withPopulation(10).build()
     val nodeB = Node.withPopulation(5).build()
     val nodeC = Node.withPopulation(8).build()
+    val nodeD = Node.withPopulation(8).build()
 
     val nodes = Map(
       "A" -> nodeA,
       "B" -> nodeB,
-      "C" -> nodeC
+      "C" -> nodeC,
+      "D" -> nodeD
     )
 
     val edges = Set(
       Edge("A", "B", EdgeType.Land),
       Edge("A", "B", EdgeType.Air),
       Edge("A", "B", EdgeType.Sea),
-      Edge("B", "C", EdgeType.Sea)
+      Edge("B", "C", EdgeType.Sea),
+      Edge("D", "C", EdgeType.Sea),
+      Edge("C", "D", EdgeType.Land),
+      Edge("D", "A", EdgeType.Land, weight = 2.0)
+
     )
 
     World(
