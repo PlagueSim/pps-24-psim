832b80c6068e5345a4289c080e0167684375a2b4
Andrea Zavatta <zavattaandrea@gmail.com>
2025-07-03 15:55:25 +0200

    PS-1: add nodes to the WorldFactory to see how it display 15 nodes

--- src/main/scala/model/World/WorldFactory.scala
+++ src/main/scala/model/World/WorldFactory.scala
@@ -1,28 +1,36 @@
 package model.World
 
 object WorldFactory:
-  def mockWorld(): World =
-    val nodeA = Node.withPopulation(10).build()
-    val nodeB = Node.withPopulation(5).build()
-    val nodeC = Node.withPopulation(8).build()
-    val nodeD = Node.withPopulation(8).build()
 
-    val nodes = Map(
-      "A" -> nodeA,
-      "B" -> nodeB,
-      "C" -> nodeC,
-      "D" -> nodeD
-    )
+  def mockWorld(): World =
+    val nodes =
+      (1 to 15).map { i =>
+        val id = ('A' + (i - 1)).toChar.toString
+        id -> Node.withPopulation(10 + i).build()
+      }.toMap
 
     val edges = Set(
       Edge("A", "B", EdgeType.Land),
-      Edge("A", "B", EdgeType.Air),
-      Edge("A", "B", EdgeType.Sea),
       Edge("B", "C", EdgeType.Sea),
-      Edge("D", "C", EdgeType.Sea),
-      Edge("C", "D", EdgeType.Land),
-      Edge("D", "A", EdgeType.Land, weight = 2.0)
-
+      Edge("C", "D", EdgeType.Air),
+      Edge("D", "E", EdgeType.Land),
+      Edge("E", "F", EdgeType.Sea),
+      Edge("F", "G", EdgeType.Air),
+      Edge("G", "H", EdgeType.Land),
+      Edge("H", "I", EdgeType.Sea),
+      Edge("I", "J", EdgeType.Air),
+      Edge("J", "K", EdgeType.Land),
+      Edge("K", "L", EdgeType.Sea),
+      Edge("L", "M", EdgeType.Air),
+      Edge("M", "N", EdgeType.Land),
+      Edge("N", "O", EdgeType.Sea),
+      Edge("O", "A", EdgeType.Air), // wrap around to A
+      // Some extra cross connections:
+      Edge("A", "H", EdgeType.Sea),
+      Edge("C", "I", EdgeType.Land),
+      Edge("D", "K", EdgeType.Air),
+      Edge("F", "M", EdgeType.Sea),
+      Edge("G", "O", EdgeType.Land)
     )
 
     World(
