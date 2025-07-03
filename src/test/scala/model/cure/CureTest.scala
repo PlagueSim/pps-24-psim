dcbf1c5fe72c2735d01872b09b1bfe8c5cbcc3df
Matteo Susca <matteo.susca@studio.unibo.it>
2025-07-03 10:50:59 +0200

    PS-22: CureTest Refactor

--- src/test/scala/model/cure/CureTest.scala
+++ src/test/scala/model/cure/CureTest.scala
@@ -5,22 +5,28 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks._
 
 class CureTest extends AnyFlatSpec with Matchers:
-  def defaultCure(progress: Double = 0.0, baseSpeed: Double = 0.1, modifiers: CureModifiers = CureModifiers.empty) =
+  def defaultCure(progress: Double = 0.0, baseSpeed: Double = 0.1, modifiers: CureModifiers = CureModifiers.empty): Cure =
     Cure(progress = progress, baseSpeed = baseSpeed, modifiers = modifiers)
 
-  "A Cure" should "have correct default and custom values" in {
-    val cases = Table(
-      ("cure", "expectedProgress", "expectedBaseSpeed", "expectedModifiers"),
-      (defaultCure(), 0.0, 0.1, CureModifiers.empty),
-      (defaultCure(progress = 0.5), 0.5, 0.1, CureModifiers.empty),
-      (defaultCure(baseSpeed = 0.2), 0.0, 0.2, CureModifiers.empty),
-      (defaultCure(modifiers = CureModifiers.empty.add(CureModifier.Additive(0.1))), 0.0, 0.1, CureModifiers.empty.add(CureModifier.Additive(0.1)))
-    )
-    forAll(cases) { (cure, expectedProgress, expectedBaseSpeed, expectedModifiers) =>
-      cure.progress shouldEqual expectedProgress
-      cure.baseSpeed shouldEqual expectedBaseSpeed
-      cure.modifiers shouldEqual expectedModifiers
-    }
+  "A Cure" should "have correct default value" in {
+    val cure = defaultCure()
+    cure.progress shouldEqual 0.0
+  }
+
+  it should "have correct custom progress value" in {
+    val cure = defaultCure(progress = 0.5)
+    cure.progress shouldEqual 0.5
+  }
+
+  it should "have correct custom baseSpeed value" in {
+    val cure = defaultCure(baseSpeed = 0.2)
+    cure.baseSpeed shouldEqual 0.2
+  }
+
+  it should "have correct custom modifiers value" in {
+    val mods = CureModifiers.empty.add(CureModifier.Additive(0.1))
+    val cure = defaultCure(modifiers = mods)
+    cure.modifiers shouldEqual mods
   }
 
   it should "allow adding and removing modifiers" in {
