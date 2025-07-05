254ec9333306117d0629310f9cda87b63b571394
Andrea Zavatta <zavattaandrea@gmail.com>
2025-07-05 15:59:40 +0200

    PS-31: add CiruclarLayout, the implementation of the position of the nodes when the program starts

---
+++ src/main/scala/view/World/CircularLayout.scala
@@ -0,0 +1,19 @@
+package view
+
+import scala.math.*
+
+class CircularLayout(
+                      radius: Double = 200,
+                      centerX: Double = 400,
+                      centerY: Double = 250
+                    ) extends GraphLayout:
+  def computePositions(nodeIds: Seq[String]): Map[String, (Double, Double)] =
+    if nodeIds.isEmpty then Map.empty
+    else
+      val angleStep = 2 * Pi / nodeIds.size
+      nodeIds.zipWithIndex.map { (id, i) =>
+        val angle = i * angleStep
+        val x = centerX + radius * cos(angle)
+        val y = centerY + radius * sin(angle)
+        id -> (x, y)
+      }.toMap
