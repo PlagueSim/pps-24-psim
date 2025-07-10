5f806f9bd604443ec7818eac14c0801f60730a55
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-07-10 16:25:52 +0200

    PS-37 new event trait with updated methods

--- src/main/scala/model/core/SimulationState.scala
+++ src/main/scala/model/core/SimulationState.scala
@@ -3,17 +3,11 @@ package model.core
 import model.cure.Cure
 import model.plague.Disease
 import model.time.Time
-import monocle.Lens
-import monocle.macros.GenLens
 
-final case class SimulationState(time: Time, disease: Disease, cure: Cure)
-
-object SimulationState:
-  val currentTimeLens: Lens[SimulationState, Time] =
-    GenLens[SimulationState](_.time)
-
-  val currentDiseaseLens: Lens[SimulationState, Disease] =
-    GenLens[SimulationState](_.disease)
-
-  val currentCureLens: Lens[SimulationState, Cure] =
-    GenLens[SimulationState](_.cure)
+//fix replace not type-safe
+case class SimulationState(time: Time, disease: Disease, cure: Cure):
+  def replace[A](newValue: A): SimulationState = newValue match
+    case newTime: Time       => this.copy(time = newTime)
+    case newDisease: Disease => this.copy(disease = newDisease)
+    case newCure: Cure       => this.copy(cure = newCure)
+    case _                   => this
