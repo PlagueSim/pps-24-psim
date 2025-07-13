0477d07a7f797850440e632ef99a83f73e6afb77
Matteo Susca <matteo.susca@studio.unibo.it>
2025-07-13 19:09:49 +0200

    PS-44: LinearInfectedThresholdEvent created and tests

--- src/main/scala/model/events/CureEvents.scala
+++ src/main/scala/model/events/CureEvents.scala
@@ -13,3 +13,22 @@ import cats.data.State
 case class BasicCureEvent() extends Event[Cure]:
   override def modifyFunction(state: SimulationState): Cure =
     state.cure.advance()
+
+case class LinearInfectedThresholdEvent(threshold: Double = 0.5)
+    extends Event[Cure]:
+  override def modifyFunction(state: SimulationState): Cure =
+    val nodeModifiers = state.world.nodes.collect:
+      case (nodeId, node)
+          if node.population > 0 && node.infected.toDouble / node.population > threshold =>
+        val modId = model.cure.ModifierId(
+          model.cure.ModifierSource.Node(model.cure.NodeId(nodeId)),
+          model.cure.ModifierKind.Additive
+        )
+        modId -> model.cure.CureModifier.Additive(modId, 0.01)
+        
+    val missingModifiers = nodeModifiers.filterNot:
+      case (modId, _) =>
+        state.cure.modifiers.modifiers.contains(modId)
+
+    missingModifiers.values.foldLeft(state.cure): (cure, mod) =>
+      cure.copy(modifiers = cure.modifiers.add(mod))
