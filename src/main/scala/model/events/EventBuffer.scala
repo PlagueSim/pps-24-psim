e361ee82ef6b00ba537d38e84b8c83b2651c9731
Alessandro Stefani <sandostef@gmail.com>
2025-07-19 16:46:18 +0200

    PS-46: a bit of scaladoc

--- src/main/scala/model/events/EventBuffer.scala
+++ src/main/scala/model/events/EventBuffer.scala
@@ -4,17 +4,26 @@ import model.cure.Cure
 import model.plague.Disease
 
 /**
- *
- * @param eventType
+ *  A generic event buffer that needs to be extended to have a specified return type
+ *  in [[modifyFunction]]
+ * @param eventType default [[SimulationState]], needs to be specified between one of
+ *                  its own parameters
  * @tparam A
  *   The type of result produced when the event is executed.
  */
 private case class EventBuffer[A](eventType: SimulationState => A) extends Event[A]:
   private var eventList: List[Event[A]] = List.empty
 
+  /**
+   * adds an event to the queue
+   * @param event the [[Event]] to add to the queue
+   */
   def newEvent(event: Event[A]): Unit = this.synchronized:
     eventList = eventList :+ event
 
+  /**
+   * called by the engine to update the [[SimulationState]]
+   */
   override def modifyFunction(state: SimulationState): A = this.synchronized:
     eventList.foreach(e => e.modifyFunction(state))
     eventList match
@@ -25,8 +34,14 @@ private case class EventBuffer[A](eventType: SimulationState => A) extends Event
         event.modifyFunction(state)
       case _ => eventType(state)
 
+/**
+ * The specific buffer only for [[Event]] that update [[Disease]]
+ */
 object DiseaseEventBuffer extends EventBuffer[Disease](_.disease)
 
+/**
+ * The specific buffer only for [[Event]] that update [[Cure]]
+ */
 object CureEventBuffer extends EventBuffer[Cure](_.cure)
 
 
