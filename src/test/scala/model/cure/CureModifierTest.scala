f7f57758f01a7b7f3f10aa6b68e0360a63a6b858
Matteo Susca <matteo.susca@studio.unibo.it>
2025-07-22 17:47:52 +0200

    PS-64: CureEvent Tests and reformat

--- src/test/scala/model/cure/CureModifierTest.scala
+++ src/test/scala/model/cure/CureModifierTest.scala
@@ -5,13 +5,18 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class CureModifierTest extends AnyFlatSpec with Matchers:
-  val nodeId = ModifierId(ModifierSource.Node(NodeId("testNode")), ModifierKind.Multiplier)
-  val mutationId = ModifierId(ModifierSource.Mutation(MutationId("testMutation")), ModifierKind.Additive)
-  val globalId = ModifierId(ModifierSource.Global, ModifierKind.ProgressModifier)
+  val nodeId =
+    ModifierId(ModifierSource.Node(NodeId("testNode")), ModifierKind.Multiplier)
+  val mutationId = ModifierId(
+    ModifierSource.Mutation(MutationId("testMutation")),
+    ModifierKind.Additive
+  )
+  val globalId =
+    ModifierId(ModifierSource.Global, ModifierKind.ProgressModifier)
 
   "A CureModifier" should "correctly implement modifier types" in:
-    val multiplier = Multiplier(nodeId, 2.0)
-    val additive = Additive(mutationId, 0.05)
+    val multiplier  = Multiplier(nodeId, 2.0)
+    val additive    = Additive(mutationId, 0.05)
     val progressMod = ProgressModifier(globalId, -0.1)
 
     multiplier shouldBe a[PersistentModifier]
@@ -33,9 +38,12 @@ class CureModifierTest extends AnyFlatSpec with Matchers:
     ProgressModifier(globalId, -1.0)(0.5) shouldEqual 0.0 +- 0.00001
 
   "ModifierId" should "implement proper equality" in:
-    val id1 = ModifierId(ModifierSource.Node(NodeId("A")), ModifierKind.Additive)
-    val id2 = ModifierId(ModifierSource.Node(NodeId("A")), ModifierKind.Additive)
-    val id3 = ModifierId(ModifierSource.Node(NodeId("B")), ModifierKind.Additive)
+    val id1 =
+      ModifierId(ModifierSource.Node(NodeId("A")), ModifierKind.Additive)
+    val id2 =
+      ModifierId(ModifierSource.Node(NodeId("A")), ModifierKind.Additive)
+    val id3 =
+      ModifierId(ModifierSource.Node(NodeId("B")), ModifierKind.Additive)
 
     id1 shouldEqual id2
     id1 should not equal id3
--- src/test/scala/model/cure/CureTest.scala
+++ src/test/scala/model/cure/CureTest.scala
@@ -6,16 +6,19 @@ import org.scalatest.matchers.should.Matchers
 
 class CureTest extends AnyFlatSpec with Matchers:
 
-  val nodeId = ModifierId(ModifierSource.Node(NodeId("n1")), ModifierKind.Multiplier)
-  val mutationId = ModifierId(ModifierSource.Mutation(MutationId("m1")), ModifierKind.Additive)
-  val globalId = ModifierId(ModifierSource.Global, ModifierKind.ProgressModifier)
+  val nodeId =
+    ModifierId(ModifierSource.Node(NodeId("n1")), ModifierKind.Multiplier)
+  val mutationId =
+    ModifierId(ModifierSource.Mutation(MutationId("m1")), ModifierKind.Additive)
+  val globalId =
+    ModifierId(ModifierSource.Global, ModifierKind.ProgressModifier)
 
-  val multiplier = Multiplier(nodeId, 2.0)
-  val additive = Additive(mutationId, 0.03)
+  val multiplier  = Multiplier(nodeId, 2.0)
+  val additive    = Additive(mutationId, 0.03)
   val progressMod = ProgressModifier(globalId, -0.15)
 
   "Cure" should "correctly advance progress" in {
-    val cure = Cure(progress = 0.3, baseSpeed = 0.1)
+    val cure     = Cure(progress = 0.3, baseSpeed = 0.1)
     val advanced = cure.advance()
 
     advanced.progress shouldEqual 0.4
@@ -45,8 +48,12 @@ class CureTest extends AnyFlatSpec with Matchers:
   }
 
   it should "clamp progress after one-time modification" in {
-    Cure(progress = 0.1).addModifier(ProgressModifier(globalId, -0.2)).progress shouldEqual 0.0
-    Cure(progress = 0.9).addModifier(ProgressModifier(globalId, 0.2)).progress shouldEqual 1.0
+    Cure(progress = 0.1)
+      .addModifier(ProgressModifier(globalId, -0.2))
+      .progress shouldEqual 0.0
+    Cure(progress = 0.9)
+      .addModifier(ProgressModifier(globalId, 0.2))
+      .progress shouldEqual 1.0
   }
 
   it should "remove modifiers correctly" in {
