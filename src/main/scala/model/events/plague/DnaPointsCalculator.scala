4786db5792a1f383411f6a6e8b00ed5a20ecc82f
Alessandro Stefani <sandostef@gmail.com>
2025-07-24 15:42:41 +0200

    PS-62: extracted functions

--- src/main/scala/model/events/plague/DnaPointsCalculator.scala
+++ src/main/scala/model/events/plague/DnaPointsCalculator.scala
@@ -4,32 +4,43 @@ import model.world.Node
 import scala.math.{floor, sqrt}
 
 object DnaPointsCalculator:
+  private type Infected = Int
+  private type Deceased = Int
+  private type World = Map[String, Node]
 
   private val NewNodeDnaMul = 5
   private val AffectedPopDnaRatio = 10
 
+  private def extractDiff(prev: World, current: World): (Infected, Deceased) =
+    val prevInfectedPop = prev.values.map(_.infected).sum
+    val currentInfectedPop = current.values.map(_.infected).sum
+
+    val prevDeceasedPop = prev.values.map(_.died).sum
+    val currentDeceasedPop = current.values.map(_.died).sum
+
+    (Math.max(currentInfectedPop - prevInfectedPop, 0), currentDeceasedPop - prevDeceasedPop)
+
+  private def newInfectedNodes(prev: World, current: World): Int =
+    val prevInfectedNodes = prev.collect {
+      case (name, node) if node.infected > 0 => name
+    }.toSet
+
+    val currentInfectedNodes = current.collect {
+      case (name, node) if node.infected > 0 => name
+    }.toSet
+
+    (currentInfectedNodes -- prevInfectedNodes).size
+
+
   def calculate(
-                 prevNodes: Map[String, Node],
-                 currentNodes: Map[String, Node]
+                 prevNodes: World,
+                 currentNodes: World
                ): Int =
-    val prevInfectedPop = prevNodes.values.map(_.infected).sum
-    val currentInfectedPop = currentNodes.values.map(_.infected).sum
-    val newInfectedPop = Math.max(currentInfectedPop - prevInfectedPop, 0)
+    val (newInfectedPop, newDeceasedPop) = extractDiff(prevNodes, currentNodes)
 
-    val prevDeceasedPop = prevNodes.values.map(_.died).sum
-    val currentDeceasedPop = currentNodes.values.map(_.died).sum
-    val newDeceasedPop = currentDeceasedPop - prevDeceasedPop
+    val redBubbles = newInfectedNodes(prevNodes, currentNodes)
 
-    val prevInfectedNodes = prevNodes.collect {
-      case (name, node) if node.infected > 0 => name
-    }.toSet
-
-    val currentInfectedNodes = currentNodes.collect {
-      case (name, node) if node.infected > 0 => name
-    }.toSet
-
-    val newInfectedNodes = currentInfectedNodes -- prevInfectedNodes
-
-    newInfectedNodes.size * NewNodeDnaMul +
+    //todo: refine the calculation
+    redBubbles * NewNodeDnaMul +
       floor(sqrt(newInfectedPop)).toInt / AffectedPopDnaRatio +
       floor(sqrt(newDeceasedPop)).toInt / AffectedPopDnaRatio
