4705945f198f1d40a9963c7c002f477b16a6f2aa
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-07-24 13:41:44 +0200

    PS-67 create prolog engine

---
+++ src/main/scala/PrologEngine.scala
@@ -0,0 +1,28 @@
+import alice.tuprolog.*
+import alice.tuprolog
+
+object PrologEngine:
+  given Conversion[String, Term] = Term.createTerm(_)
+  given Conversion[Seq[_], Term] = _.mkString("[", ",", "]")
+
+  def extractTerm(t: Term, i: Integer): Term =
+    t.asInstanceOf[Struct].getArg(i).getTerm
+
+  def mkPrologEngine(clauses: String*): Term => LazyList[Term] =
+    val engine = Prolog()
+    engine.setTheory(Theory(clauses mkString " "))
+    goal =>
+      new Iterable[Term]:
+        override def iterator: Iterator[Term] = new Iterator[Term]:
+          var solution: SolveInfo       = engine.solve(goal);
+          override def hasNext: Boolean =
+            solution != null && (solution.isSuccess || solution.hasOpenAlternatives)
+          override def next(): Term =
+            if solution == null then
+              throw new NoSuchElementException("No more solutions")
+            val current = solution.getSolution
+            solution =
+              if solution.hasOpenAlternatives then engine.solveNext
+              else null
+            current
+      .to(LazyList)
