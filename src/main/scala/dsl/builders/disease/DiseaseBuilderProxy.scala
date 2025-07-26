b7cfcf5559bd0cdd518512575a41b1bf1c38f93e
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-07-26 10:35:21 +0200

    PS-73 refactor

--- src/main/scala/dsl/builders/disease/DiseaseBuilderProxy.scala
+++ src/main/scala/dsl/builders/disease/DiseaseBuilderProxy.scala
@@ -4,23 +4,20 @@ class DiseaseBuilderProxy(
     get: () => DiseaseBuilder,
     set: DiseaseBuilder => Unit
 ) extends DiseaseBuilder:
-  override def withName(name: String): DiseaseBuilder = {
+  override def withName(name: String): DiseaseBuilder =
     val updated = get().withName(name)
     set(updated)
     updated
-  }
 
-  override def withTraits(traits: Set[model.plague.Trait]): DiseaseBuilder = {
+  override def withTraits(traits: Set[model.plague.Trait]): DiseaseBuilder =
     val updated = get().withTraits(traits)
     set(updated)
     updated
-  }
 
-  override def withPoints(points: Int): DiseaseBuilder = {
+  override def withPoints(points: Int): DiseaseBuilder =
     val updated = get().withPoints(points)
     set(updated)
     updated
-  }
 
   override def build(): model.plague.Disease =
     get().build()
