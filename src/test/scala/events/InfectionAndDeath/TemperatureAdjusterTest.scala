c62f2c793dec07f8e61e9728364741da38c2aaa3
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-07-27 12:02:45 +0200

    PS-73 remove magic numbers

--- src/test/scala/events/InfectionAndDeath/TemperatureAdjusterTest.scala
+++ src/test/scala/events/InfectionAndDeath/TemperatureAdjusterTest.scala
@@ -7,20 +7,30 @@ import org.scalatest.matchers.should.Matchers
 
 class TemperatureAdjusterTest extends AnyFlatSpec with Matchers:
 
+  private val MIN_TEMPERATURE = 10.0
+  private val MAX_TEMPERATURE = 30.0
+  private val IDEAL_TEMPERATURE = 20.0
+  private val PENALIZED_VALUE = 85.0
+  private val UNCHANGED_VALUE_1 = 50.0
+  private val UNCHANGED_VALUE_2 = 40.0
+  private val UNCHANGED_VALUE_3 = 60.0
+  private val STANDARD_VALUE = 100.0
+  private val DELTA = 0.01
+
   given adjuster: TemperatureAdjuster =
     TemperatureAdjuster.defaultTemperatureAdjuster
 
   "adjustForTemperature" should "reduce value correctly for low temperature" in:
-    adjuster.adjustForTemperature(100.0, 5.0) shouldBe 85.0 +- 0.01
+    adjuster.adjustForTemperature(STANDARD_VALUE, 5.0) shouldBe PENALIZED_VALUE +- DELTA
 
   it should "reduce value correctly for high temperature" in:
-    adjuster.adjustForTemperature(100.0, 35.0) shouldBe 85.0 +- 0.01
+    adjuster.adjustForTemperature(STANDARD_VALUE, 35.0) shouldBe PENALIZED_VALUE +- DELTA
 
   it should "return same value in ideal temperature" in:
-    adjuster.adjustForTemperature(50.0, 20.0) shouldBe 50.0 +- 0.01
+    adjuster.adjustForTemperature(UNCHANGED_VALUE_1, IDEAL_TEMPERATURE) shouldBe UNCHANGED_VALUE_1 +- DELTA
 
   it should "not penalize temperature slightly above min" in:
-    adjuster.adjustForTemperature(40.0, 10.1) shouldBe 40.0 +- 0.01
+    adjuster.adjustForTemperature(UNCHANGED_VALUE_2, MIN_TEMPERATURE + DELTA) shouldBe UNCHANGED_VALUE_2 +- DELTA
 
   it should "not penalize temperature slightly below max" in:
-    adjuster.adjustForTemperature(60.0, 29.9) shouldBe 60.0 +- 0.01
+    adjuster.adjustForTemperature(UNCHANGED_VALUE_3, MAX_TEMPERATURE - DELTA) shouldBe UNCHANGED_VALUE_3 +- DELTA
