47f9ea6edfd9d40bce9def7c1ac92319a6121ddc
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-07-28 15:00:14 +0200

    PS-93 event doc

--- src/main/scala/model/events/time/AdvanceDayEvent.scala
+++ src/main/scala/model/events/time/AdvanceDayEvent.scala
@@ -4,5 +4,8 @@ import model.core.SimulationState
 import model.events.Event
 import model.time.Time
 
+/**
+ * Advances the simulation time by one day.
+ */
 case class AdvanceDayEvent() extends Event[Time]:
   override def modifyFunction(s: SimulationState): Time = s.time + 1
