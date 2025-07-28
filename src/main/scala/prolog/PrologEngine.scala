08618c8c48b536812eea5bcfcfe3199aaca3c99b
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-07-28 15:03:46 +0200

    PS-93 prolog and view doc

--- src/main/scala/prolog/PrologEngine.scala
+++ src/main/scala/prolog/PrologEngine.scala
@@ -3,13 +3,22 @@ package prolog
 import alice.tuprolog
 import alice.tuprolog.*
 
+/**
+ * Provides a Prolog engine for solving goals and extracting terms.
+ */
 object PrologEngine:
   given Conversion[String, Term] = Term.createTerm(_)
   given Conversion[Seq[_], Term] = _.mkString("[", ",", "]")
 
+  /**
+   * Extracts a term from a Prolog `Struct` at the specified index.
+   */
   def extractTerm(t: Term, i: Integer): Term =
     t.asInstanceOf[Struct].getArg(i).getTerm
 
+  /**
+   * Creates a Prolog engine that can solve goals based on provided clauses.
+   */
   def mkPrologEngine(clauses: String*): Term => LazyList[Term] =
     val engine = Prolog()
     engine.setTheory(Theory(clauses mkString " "))
--- src/main/scala/view/updatables/UpdatableView.scala
+++ src/main/scala/view/updatables/UpdatableView.scala
@@ -2,5 +2,12 @@ package view.updatables
 
 import model.core.SimulationState
 
+/**
+ * Represents a view that will be updated with the new information from the simulation state.
+ * This trait is used to define views that need to reflect changes in the simulation state.
+ */
 trait UpdatableView:
+    /**
+     * Updates the view component with the new simulation state information.
+     */
   def update(newState: SimulationState): Unit
