6341d7480fc310d59273361a9102c04ea2e9d6d3
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-07-31 18:46:36 +0200

    PS-102 rename class and indent

--- src/main/scala/model/infection/PopulationEffect.scala
+++ src/main/scala/model/infection/PopulationEffect.scala
@@ -3,7 +3,9 @@ package model.infection
 import model.plague.Disease
 import model.world.Node
 
-/** Represents a population effect that can be applied to a node based on a disease */
+/** Represents a population effect that can be applied to a node based on a
+  * disease
+  */
 trait PopulationEffect:
-    /** Applies the population effect to a given node based on the disease*/
+  /** Applies the population effect to a given node based on the disease */
   def applyToPopulation(node: Node, disease: Disease): Node
