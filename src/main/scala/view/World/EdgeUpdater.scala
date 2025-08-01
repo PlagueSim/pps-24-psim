3ab0a43468d93b77e4074396f51c78928df0f5b1
Andrea Zavatta <zavattaandrea@gmail.com>
2025-08-01 17:32:00 +0200

    PS-99: refactor edgeUpdater

--- src/main/scala/view/World/EdgeUpdater.scala
+++ src/main/scala/view/World/EdgeUpdater.scala
@@ -3,49 +3,48 @@ package view.world
 import javafx.scene.shape.Line
 import model.world.Edge
 import model.world.EdgeExtensions.*
-import view.world.EdgeLayer.updateLine
-import view.world.EdgeLayer.createEdgeLineSafe
+import view.world.EdgeLayer.{updateLine, createEdgeLineSafe}
 
 object EdgeUpdater:
-  /**
-   * @param currentEdges A map of current edges in the scene, where keys are edge IDs and values are Line instances.
-   * @param updatedEdges An iterable of updated edges, which may include new edges or modifications to existing ones.
-   * @param nodePositions A map of node positions, where keys are node IDs and values are their positions.
-   * 
-   * @return A tuple containing:
-   *         - A map of updated edges with their corresponding Line instances.
-   *         - A set of new Line instances to be added to the scene.
-   *         - A set of old Line instances to be removed from the scene.
-   * This method ensures that the visual representation of edges is kept in sync with the logical state of the world.
-   * */
 
+  /**
+   * Updates the visual representation of edges based on the new state.
+   *
+   * @param currentEdges Current edge lines in the scene.
+   * @param updatedEdges Updated edge data from the model.
+   * @param nodePositions Node positions for drawing lines.
+   * @return (updated edge map, new lines to add, old lines to remove)
+   */
   def update(
               currentEdges: Map[String, Line],
               updatedEdges: Iterable[Edge],
               nodePositions: Map[String, LivePosition]
-            ): (Map[String, Line], Set[Line], Set[Line]) = {
+            ): (Map[String, Line], Set[Line], Set[Line]) =
 
-    val updatedMap = scala.collection.mutable.Map[String, Line]()
-    val toAdd = scala.collection.mutable.Set[Line]()
-    val seenIds = scala.collection.mutable.Set[String]()
+    val (updatedMap, toAdd, seenIds) = updatedEdges.foldLeft((Map.empty[String, Line], Set.empty[Line], Set.empty[String])) {
+      case ((accMap, accAdd, accSeen), edge) =>
+        val id = edge.edgeId
+        val (lineOpt, newLines) = updateOrCreateLine(edge, currentEdges, nodePositions)
+        lineOpt match
+          case Some(line) => (accMap + (id -> line), accAdd ++ newLines, accSeen + id)
+          case None       => (accMap, accAdd, accSeen + id)
+    }
 
-    for edge <- updatedEdges do
-      val id = edge.edgeId
-      seenIds += id
-      val line = currentEdges.get(id) match
-        case Some(existing) =>
-          EdgeLayer.updateLine(existing, edge, nodePositions)
-          existing
-        case None =>
-          EdgeLayer.createEdgeLineSafe(edge, nodePositions) match
-            case Some(newLine) =>
-              toAdd += newLine
-              newLine
-            case None => null
+    val toRemove = currentEdges.filterNot((id, _) => seenIds.contains(id)).values.toSet
 
-      if line != null then updatedMap(id) = line
+    (updatedMap, toAdd, toRemove)
 
-    val toRemove = currentEdges.filterNot { case (id, _) => seenIds.contains(id) }.values.toSet
+  private def updateOrCreateLine(
+                                  edge: Edge,
+                                  currentEdges: Map[String, Line],
+                                  nodePositions: Map[String, LivePosition]
+                                ): (Option[Line], Set[Line]) =
+    currentEdges.get(edge.edgeId) match
+      case Some(existing) =>
+        updateLine(existing, edge, nodePositions)
+        (Some(existing), Set.empty)
 
-    (updatedMap.toMap, toAdd.toSet, toRemove)
-  }
+      case None =>
+        createEdgeLineSafe(edge, nodePositions) match
+          case Some(newLine) => (Some(newLine), Set(newLine))
+          case None          => (None, Set.empty)
