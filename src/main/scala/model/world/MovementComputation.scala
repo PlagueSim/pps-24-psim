79a1b3e874a6e435616ca5deed043898157c7f1a
Andrea Zavatta <zavattaandrea@gmail.com>
2025-08-04 09:51:30 +0200

    PS-106: fix bug

--- src/main/scala/model/world/MovementComputation.scala
+++ src/main/scala/model/world/MovementComputation.scala
@@ -51,7 +51,7 @@ object MovementComputation:
     val fromNode = nodes(from)
     if fromNode.population <= 0 then return nodes
 
-    val infectedMoving = sampleInfected(fromNode, amount)
+    val infectedMoving = sampleInfected(fromNode, amount.min(fromNode.population))
 
     val updatedFrom = fromNode
       .decreasePopulation(amount)
