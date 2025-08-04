604b6a0728de36c2ff74bfe7fef2bd37e8d2b12c
Matteo Susca <matteo.susca@studio.unibo.it>
2025-08-04 14:48:39 +0200

    PS-106: removed not working infectionTypes

--- src/main/scala/model/infection/InfectionTypes.scala
+++ src/main/scala/model/infection/InfectionTypes.scala
@@ -68,68 +68,6 @@ object InfectionTypes:
       changeApplier = (node, infected) => node.increaseInfection(infected)
     )
 
-  /** Advanced probabilistic infection logic that allows for a variable number
-    * of affected individuals
-    */
-  def AdvancedProbabilistic(affectable: Int): PopulationEffect =
-    PopulationEffectComposer.apply(
-      canApply = STANDARD_CAN_APPLY,
-      parameterExtractor = _.infectivity,
-      populationSelector = node => {
-        val totalAffected =
-          try {
-            Math.multiplyExact(node.infected, affectable)
-          } catch {
-            case e: ArithmeticException =>
-              println(
-                s"Overflow occurred while calculating totalAffected: ${e.getMessage}"
-              )
-              Int.MaxValue // Fallback to a safe value
-          }
-        val hyp = new HypergeometricDistribution(
-          node.population,
-          node.population - node.infected,
-          totalAffected
-        )
-        hyp.sample()
-      },
-      changeCalculator = (healthy, prob) =>
-        new BinomialDistribution(healthy, prob.value).sample(),
-      changeApplier = (node, infected) => node.increaseInfection(infected)
-    )
-
-  def SimplifiedProbabilistic(affectable: Int): PopulationEffect =
-    PopulationEffectComposer.apply(
-      canApply = STANDARD_CAN_APPLY,
-      parameterExtractor = _.infectivity,
-      populationSelector = node => {
-        val healthy = node.population - node.infected
-        val p = 1.0 - pow((node.population - 1 - affectable).toDouble / (node.population - 1).toDouble, node.infected.toDouble)
-        BinomialDistribution(healthy, p * 0.1).sample()
-      },
-      changeCalculator = (healthy, prob) =>
-        new BinomialDistribution(healthy, prob.value).sample(),
-      changeApplier = (node, infected) => node.increaseInfection(infected)
-    )
-
-  val StandardInfection2: PopulationEffect =
-    PopulationEffectComposer.apply(
-      canApply = STANDARD_CAN_APPLY,
-      parameterExtractor = _.infectivity,
-      populationSelector = node => {
-        val infected = node.infected
-        println(s"StandardInfection2: Infected = $infected")
-        infected
-      },
-      changeCalculator = (healthy, prob) => {
-        println(s"prob.value = ${prob.value}, healthy = $healthy")
-        BinomialDistribution((healthy * (prob.value * 100)).toInt, prob.value).sample()
-      },
-      changeApplier = (node, infected) => node.increaseInfection(infected)
-    )
-
-  // Add to InfectionTypes.scala
-
   val SIRLogic: PopulationEffect =
     PopulationEffectComposer.apply[(Int,Int)](
       canApply = STANDARD_CAN_APPLY,
